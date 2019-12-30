58e593fa3312e1edd391b02df2335e93018f2734
Josh Mabry <mabry1985@gmail.com>
2019-12-30 15:42:16 -0800

    update model

--- server/models/tutorial-model.js
+++ server/models/tutorial-model.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema
 const Tutorial = new Schema(
   {
     title: { type: String, required: true },
-    // channel: { type: String, required: true },
-    // description: { type: String, required: true },
-    // date: { type: String, required: true },
-    // tags: { type: [String], required: true },
-    // embedUrl: { type: String, required: true }
-    // playlist: { type: Boolean, required: true },
+    channel: { type: String, required: true },
+    description: { type: String, required: true },
+    date: { type: String, required: true },
+    embedUrl: { type: String, required: true },
+    tags: { type: [String], required: false },
+    playlist: { type: Boolean, required: false },
+    comments: { type: [{}], required: false },
   },
   { timestamps: true },
 )
