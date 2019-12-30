930e0705a862727841569775da93b6eac5022c2a
Josh Mabry <mabry1985@gmail.com>
2019-12-30 14:57:49 -0800

    add youtube scraper

---
+++ server/scrapers/youtubeScraper.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const sample = {
+    title: "Web Development Tutorial",
+    channel: "Net Ninja",
+    description: { type: String, required: true },
+    date: { type: String, required: true },
+    tags: ["Javascript", "Web", "React"],
+    embedUrl: `https://www.youtube.com/embed/GUEB9FogoP8`,
+    playlist: false,
+};
+
+async function scrapeYoutube(videoCode, arr) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  const url = `https://www.youtube.com/watch?v=${videoCode}`
+  await page.goto(url, { waitUntil: "networkidle2" }); 
+  const html = await page.evaluate(() => document.body.innerHTML);
+  fs.writeFileSync("./test.html", html);
+  const $ = cheerio.load(html);
+
+  const title = $("h1.title").text().trim();
+  const channel = $("#channel-name a").text();
+  const description = $("#description").text().trim();
+  const date = $("#date > yt-formatted-string").text();
+  const embedUrl = `https://www.youtube.com/embed/${videoCode}`;
+  const tags = arr;
+
+  const tutorial = {
+    title,
+    channel,
+    description,
+    date,
+    embedUrl,
+    tags
+  }
+
+  console.log(tutorial)
+  return tutorial
+}
+
+const arr = ["Javascript", "Web", "React"];
+
+scrapeYoutube("GUEB9FogoP8", arr);
