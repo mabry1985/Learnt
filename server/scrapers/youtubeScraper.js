500cc47082797928d235bb5de3867641e67b5fa2
Josh Mabry <mabry1985@gmail.com>
2020-01-02 16:46:16 -0800

    refactor scraper

--- server/scrapers/youtubeScraper.js
+++ server/scrapers/youtubeScraper.js
@@ -2,17 +2,7 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const sample = {
-    title: "Web Development Tutorial",
-    channel: "Net Ninja",
-    description: { type: String, required: true },
-    date: { type: String, required: true },
-    tags: ["Javascript", "Web", "React"],
-    embedUrl: `https://www.youtube.com/embed/GUEB9FogoP8`,
-    playlist: false,
-};
-
-async function scrapeYoutube(videoCode, arr, bool) {
+async function scrapeYoutube(videoCode) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const url = `https://www.youtube.com/watch?v=${videoCode}`
@@ -26,8 +16,7 @@ async function scrapeYoutube(videoCode, arr, bool) {
   const description = $("#description").text().trim();
   const date = $("#date > yt-formatted-string").text();
   const embedUrl = `https://www.youtube.com/embed/${videoCode}`;
-  const tags = arr;
-  const playlist = bool;
+  const watchUrl = url;
 
   const tutorial = {
     title,
@@ -35,15 +24,11 @@ async function scrapeYoutube(videoCode, arr, bool) {
     description,
     date,
     embedUrl,
-    tags,
-    playlist
+    watchUrl
   }
 
-  console.log(tutorial)
   return tutorial
 }
-
-const arr = ["Javascript", "Web", "React"];
 
 module.exports = {
   scrapeYoutube
