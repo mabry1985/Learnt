4244bf8263abf34e90b202ce178e34665ba23163
Josh Mabry <mabry1985@gmail.com>
2020-01-05 10:20:10 -0800

    render iframe

--- client/src/pages/EditResults.jsx
+++ client/src/pages/EditResults.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import api from "../api";
 import styled from "styled-components";
+import Iframe from "react-iframe";
 
 //Styled Components==========================================
 const Wrapper = styled.div.attrs({
+  className: "form-group container"
+})`
+  margin: 0 30px;
+`;
+
+const Form = styled.div.attrs({
   className: "form-group container"
 })`
   margin: 0 30px;
@@ -35,25 +41,34 @@ const CancelButton = styled.a.attrs({
 //============================================================
 
 const EditTutorial = props => {
-  const { title, description } = props.tutorial;
+  const { title, description, embedUrl } = props.tutorial;
 
   return (
     <Wrapper>
-      <Label>Title: </Label>
-      <InputText
-        type="text"
-        defaultValue={title}
-        onChange={props.handleChangeTitle}
+      <Iframe
+        url={embedUrl}
+        width="700px"
+        height="395px"
+        className="video"
+        display="initial"
+        position="relative"
       />
-      <Label>Description: </Label>
-      <InputText
-        type="text"
-        defaultValue={description}
-        onChange={props.handleChangeDefinition}
-      />
-
-      <Button onClick={props.onCreateTutorial}>Create Tutorial</Button>
-      <CancelButton onClick={props.onResetState}>Cancel</CancelButton>
+      <Form>
+        <Label>Title: </Label>
+        <InputText
+          type="text"
+          defaultValue={title}
+          onChange={props.handleChangeTitle}
+        />
+        <Label>Description: </Label>
+        <InputText
+          type="text"
+          defaultValue={description}
+          onChange={props.handleChangeDefinition}
+        />
+        <Button onClick={props.onCreateTutorial}>Create Tutorial</Button>
+        <CancelButton onClick={props.onResetState}>Cancel</CancelButton>
+      </Form>
     </Wrapper>
   );
 };
